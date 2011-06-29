59091560445e38932cb80a2608adeab5a4d86167
Frank Fejes <frank@fejes.net>
2011-06-29 12:05:27 -0500

    better time resolution

--- jdbc_test.rb
+++ jdbc_test.rb
@@ -15,7 +15,7 @@ conn.set_auto_commit false
 stmt = conn.create_statement
 stmt.fetch_size = 50
 
-begin_ts = Time.now.to_i
+begin_ts = Time.now
 
 rslt = stmt.execute_query sql
 meta = rslt.meta_data
@@ -29,6 +29,6 @@ while rslt.next do
   n += 1
 end
 
-elapsed = Time.now.to_i - begin_ts
-rate = '%0.02f' % (n / elapsed.to_f)
-STDERR.puts "#{n} rows processed in #{elapsed} seconds (#{rate} recs/s)"
+elapsed = Time.now - begin_ts
+rate = '%0.02f' % (n / elapsed)
+STDERR.puts "#{n} rows processed in #{elapsed.to_i} seconds (#{rate} recs/s)"
